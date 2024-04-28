391081795fbab312a35cd6562132c2c95bb71aa8
jdang00 <jdang00@g.uafs.edu>
2024-04-28 17:03:31 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="relative flex flex-col max-w-[1400] mx-auto w-full text-sm sm:text-base min-h-screen">
+	<h1 class="text-5xl font-bold">Justin Dang</h1>
+	<p>University of Arkansas – Fort Smith</p>
+</div>
+
+<slot />
