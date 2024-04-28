ac1f373b1d1e322a12096fef87ac6a39651b078f
Justin Dang <jdang00@g.uafs.edu>
2024-04-28 18:18:44 -0500

    added picture and styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,37 @@
 <script>
 	import '../app.css';
 	import Nav from '$lib/Nav.svelte';
+	import self from '../img/justin.webp';
 </script>
 
-<Nav/>
+<Nav />
 
-<div class="relative flex flex-col mx-auto text-center w-full my-24">
-	<h1 class="text-5xl font-bold">Justin A. Dang</h1>
-	<p class="mt-4 text-gray-600">Northeastern State University | University of Arkansas – Fort Smith</p>
-	<p class="mt-8">Lover of God, family, and learning.</p>
-
+<div class="relative flex flex-row mx-auto w-full my-24 justify-center gap-14 flex-wrap">
+	<div>
+		<img src={self} class="h-96 rounded-lg" alt="Justin Dang" />
+	</div>
+	<div class="flex flex-col">
+		<h1 class="text-5xl font-bold">Justin A. Dang</h1>
+		<p class="mt-4 text-gray-600">
+			Northeastern State University | University of Arkansas – Fort Smith
+		</p>
+		<p class="mt-8">Lover of God, family, and learning.</p>
+	</div>
 </div>
 
 <ul class="list-disc list-inside mt-6 mx-20">
-	<li>I fell in love with the classics at Logos Online School who gave me an incredible Classical Christian Education experience.</li>
-	<li>I studied computer science at UAFS, where I gained my passion for software engineering, AI, and technology.</li>
-	<li>I'm currently a student at the Oklahoma School of Optometry learning to give professional vision care for all.</li>
+	<li>
+		I fell in love with the classics at Logos Online School who gave me an incredible Classical
+		Christian Education experience.
+	</li>
+	<li>
+		I studied computer science at UAFS, where I gained my passion for software engineering, AI, and
+		technology.
+	</li>
+	<li>
+		I'm currently a student at the Oklahoma School of Optometry learning to give professional vision
+		care for all.
+	</li>
 </ul>
 
 <slot />
