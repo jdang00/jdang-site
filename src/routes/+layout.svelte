e4c3bd4667342fa9489bc8a41a1f9897a32bd14f
Justin Dang <jdang00@g.uafs.edu>
2024-04-28 17:48:52 -0500

    added content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
 	import '../app.css';
+	import Nav from '$lib/Nav.svelte';
 </script>
 
-<div class="relative flex flex-col max-w-[1400] mx-auto w-full text-sm sm:text-base min-h-screen">
-	<h1 class="text-5xl font-bold">Justin Dang</h1>
-	<p>University of Arkansas – Fort Smith</p>
+<Nav/>
+
+<div class="relative flex flex-col mx-auto text-center w-full my-24">
+	<h1 class="text-5xl font-bold">Justin A. Dang</h1>
+	<p class="mt-4 text-gray-600">Northeastern State University | University of Arkansas – Fort Smith</p>
+	<p class="mt-8">Lover of God, family, and learning.</p>
+
 </div>
+
+<ul class="list-disc list-inside mt-6 mx-20">
+	<li>I fell in love with the classics at Logos Online School who gave me an incredible Classical Christian Education experience.</li>
+	<li>I studied computer science at UAFS, where I gained my passion for software engineering, AI, and technology.</li>
+	<li>I'm currently a student at the Oklahoma School of Optometry learning to give professional vision care for all.</li>
+</ul>
 
 <slot />
