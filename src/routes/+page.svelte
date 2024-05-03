d1f2fa01bf1149ddd36190fa8a0aae32d6924b8a
Justin Dang <jdang00@g.uafs.edu>
2024-05-03 03:47:53 -0500

    gap issues and new undergrad route

---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+	import '../app.css';
+	import Nav from '$lib/Nav.svelte';
+	import self from '../img/justin.webp';
+	import CourseTable from './classes/data-table.svelte';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Nav />
+
+<div class="flex flex-col mx-auto my-24 gap-14 flex-wrap">
+	<div class="flex flex-row flex-wrap justify-center gap-x-10 text-center">
+		<div class="flex flex-col mb-10">
+			<h1 class="text-5xl font-bold">Justin A. Dang</h1>
+			<p class="mt-4 text-gray-600">Oklahoma School of Optometry | Northeastern State University</p>
+			<p class="mt-2 text-gray-600">
+				Computer Science and Mathematics | University of Arkansas – Fort Smith
+			</p>
+		</div>
+	</div>
+
+	<div class="text-left">
+		<div class="flex flex-row justify-evenly mx-10 flex-wrap gap-5">
+			<div class="max-w-sm rounded overflow-hidden shadow-lg">
+				<div class="overflow-hidden">
+					<img
+						class="hover:scale-110 transition-all duration-500 cursor-pointer"
+						src={self}
+						alt="logos"
+					/>
+				</div>
+
+				<div class="px-6 py-4">
+					<div class="font-bold text-xl mb-2">Classically Educated.</div>
+					<p class="text-gray-700 text-base">
+						I fell in love with the classics at Logos Online School who gave me an incredible
+						Classical Christian Education experience. Read more about my thoughts on homeschooling
+						and Christian thinking about cultural issues.
+					</p>
+					<Button class="mt-4" variant="outline">Read more</Button>
+				</div>
+			</div>
+			<div class="max-w-sm rounded overflow-hidden shadow-lg">
+				<div class="overflow-hidden">
+					<img
+						class="hover:scale-110 transition-all duration-500 cursor-pointer"
+						src={self}
+						alt="cs"
+					/>
+				</div>
+				<div class="px-6 py-4">
+					<div class="font-bold text-xl mb-2">Coder to the Core.</div>
+					<p class="text-gray-700 text-base">
+						I studied computer science at UAFS, where I gained my passion for software engineering,
+						AI, and technology. Learn more about my research and development projects during
+						undergraduate.
+					</p>
+					<Button class="mt-4" variant="outline" href="/undergrad">Learn more</Button>
+				</div>
+			</div>
+			<div class="max-w-sm rounded overflow-hidden shadow-lg">
+				<div class="overflow-hidden">
+					<img
+						class="hover:scale-110 transition-all duration-500 cursor-pointer"
+						src={self}
+						alt="op"
+					/>
+				</div>
+				<div class="px-6 py-4">
+					<div class="font-bold text-xl mb-2">A Vision For the Future.</div>
+					<p class="text-gray-700 text-base">
+						I'm currently a student at the Oklahoma School of Optometry learning to give
+						professional vision care for all. Keep up with my adventures in the field of optometry
+						and my plans for the future.
+					</p>
+					<Button class="mt-4" variant="outline">Learn more</Button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<slot />
---
+++ src/routes/undergrad/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../../app.css';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Button
+	variant="link"
+	href="https://www.swtimes.com/story/news/2021/09/07/uafs-students-and-staff-honor-20th-anniversary-9-11/5619043001/"
+	target="blank">2021: UAFS Students and Staff Honor 20th Anniversary of 9/11</Button
+>
+
+<Button variant="link" href="https://uafs.edu/news/2023/constitution-week.php" target="blank"
+	>2023: Constitution Week at UAFS, Highlighted by National Voter Registration Day</Button
+>
