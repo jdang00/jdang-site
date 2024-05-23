dd26614b2ad043bea11c1c44063c06cc1b335322
Justin Dang <justindang02@gmail.com>
2024-05-22 21:08:28 -0500

    removed teachers

--- src/routes/comps/courses.svelte
+++ src/routes/comps/courses.svelte
@@ -8,7 +8,6 @@
 		<Table.Row>
 			<Table.Head>Course Name</Table.Head>
 			<Table.Head>Course Number</Table.Head>
-			<Table.Head>Instructor</Table.Head>
 			<Table.Head>Term</Table.Head>
 			<Table.Head>Grade</Table.Head>
 		</Table.Row>
@@ -18,7 +17,6 @@
 			<Table.Row>
 				<Table.Cell class="font-medium">{course.coursename}</Table.Cell>
 				<Table.Cell>{course.coursenumber}</Table.Cell>
-				<Table.Cell>{course.instructor}</Table.Cell>
 				<Table.Cell>{course.semester}</Table.Cell>
 				<Table.Cell>{course.grade}</Table.Cell>
 			</Table.Row>
