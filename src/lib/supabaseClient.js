6386f2ae58efc269b50839db7018d872fee2828f
Justin Dang <justindang02@gmail.com>
2024-05-22 21:06:47 -0500

    env variable changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY} from '$env/static/public';
+import { PRIVATE_SUPABASE_URL, PRIVATE_SUPABASE_ANON_KEY} from '$env/static/private';
 
-console.log(PUBLIC_SUPABASE_URL);
 
 const supabase = createClient(
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_ANON_KEY
+  PRIVATE_SUPABASE_URL,
+  PRIVATE_SUPABASE_ANON_KEY
 ) 
 
 
