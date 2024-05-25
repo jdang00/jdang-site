23d18dd912e11081258e7a02436c086b4d44212d
Justin Dang <justindang02@gmail.com>
2024-05-25 01:26:59 -0500

    blog styling

--- src/routes/comps/blog.svelte
+++ src/routes/comps/blog.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let data;
-	console.log(data);
+	import { Badge } from '$lib/components/ui/badge';
 
 	function formatDate(inputDate: string) {
 		const months = [
@@ -25,11 +25,20 @@
 	}
 </script>
 
-<div class="mt-8">
+<div class="mt-23">
+	<div class="border-b w-full my-8"></div>
+
 	{#each data.posts as post}
-		<h1 class="text-4xl">{post.title}</h1>
-		<p class="text-gray-500">{post.description}</p>
-		<p class="text-gray-500">{formatDate(post.date)}</p>
-		<div class="mb-24"></div>
+		<a class="my-12 hover:text-sky-600" href="./">
+			<div class="flex flex-row mb-6">
+				<p class="text-gray-500 me-5">{formatDate(post.date)}</p>
+				{#each post.categories as categories}
+					<Badge variant="secondary" class="me-3">{categories}</Badge>
+				{/each}
+			</div>
+			<h1 class="text-5xl">{post.title}</h1>
+			<p class="text-gray-500">{post.description}</p>
+			<div class="border-b w-full my-8"></div>
+		</a>
 	{/each}
 </div>
