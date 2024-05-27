bc14a6163f00b3c8bc20c7a1b882660128a8d417
jdang00 <jdang00@g.uafs.edu>
2024-05-27 00:55:36 -0500

    login g footer

--- src/routes/comps/footer.svelte
+++ src/routes/comps/footer.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <div class="text-center flex items-center justify-center flex-col mt-16 text-sm">
-	<div>&copy {date} - Justin A. Dang</div>
+	<div>
+		&copy {date} - Justin A. Dang - <a class="underline" target="_blank" href="/admin">Login</a>
+	</div>
 </div>
