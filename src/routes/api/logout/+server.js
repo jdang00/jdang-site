054d76dc3f4d4f35ea5d6ca4f7b6763eb75d38b8
Justin Dang <justindang02@gmail.com>
2024-06-03 17:48:35 -0500

    cookie security

--- src/routes/api/logout/+server.js
+++ src/routes/api/logout/+server.js
@@ -3,6 +3,8 @@ import { json } from '@sveltejs/kit';
 export const POST = async ({ cookies }) => {
 
     cookies.delete('session_id', { path: '/' });
+    cookies.delete('firstname', { path: '/' });
+    cookies.delete('lastname', { path: '/' });
 
   return json({ success: true });
 };
