fa88fd29e2473189bb77652dd6e1763d7a3cb856
Justin Dang <justindang02@gmail.com>
2024-06-08 09:02:13 -0500

    slides

---
+++ src/lib/deck/presentation.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Slide from './slide.svelte';
+	import '../../app.css';
+</script>
+
+<Slide>
+	<div class="text-left">
+		<span>History of the Bible in the English Language</span>
+		<p class="text-gray-300 text-2xl">Part 1</p>
+	</div>
+</Slide>
+
+<Slide>
+	<img
+		src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg/1280px-Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg"
+		alt="The Tower of Babel"
+	/>
+</Slide>
+
+<Slide>
+	<p>Imago Dei</p>
+	<p class="text-gray-300 text-2xl">"The Image of God"</p>
+
+	<p class="text-gray-300 text-lg">Genesis 1:27</p>
+</Slide>
---
+++ src/lib/deck/slides.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Reveal from 'reveal.js';
+
+	import 'reveal.js/dist/reveal.css';
+	import 'reveal.js/dist/theme/black.css';
+
+	import Presentation from './presentation.svelte';
+
+	onMount(() => {
+		const deck = new Reveal({
+			hash: true,
+			controls: false,
+			progress: false
+		});
+
+		deck.initialize();
+	});
+</script>
+
+<div class="reveal">
+	<div class="slides">
+		<Presentation />
+	</div>
+</div>
