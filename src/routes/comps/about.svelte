e1d7e8a45f4cc28e8f32c1afea90c92fb1d7fa95
jdang00 <jdang00@g.uafs.edu>
2024-08-05 09:34:29 -0500

    bio update

--- src/routes/comps/about.svelte
+++ src/routes/comps/about.svelte
@@ -1,8 +1,8 @@
 <div class="mt-8">
 	<p class="text-gray-500">
-		Saved by grace through faith. Author of Immortology: Essays About Faith and Life. Developer
-		specializing in artificial intelligence, machine learning, and human computer interaction.
-		Future doctor of optometry. Peace if posssible, truth at all costs.
+		Saved by grace through faith. Writer of essays on faith and life. Developer with interests in
+		artificial intelligence, machine learning, and human computer interaction. Future doctor of
+		optometry. Peace if posssible, truth at all costs.
 	</p>
 
 	<div class="mt-12">
@@ -12,7 +12,7 @@
 				<div class="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border"></div>
 				<h3 class="font-medium text-lg">Doctor of Optometry (O.D.)</h3>
 				<p class="text-gray-500">Northeastern State University, Oklahoma College of Optometry</p>
-				<p class="text-gray-500 italic">Arkansas Contract Seat</p>
+				<p class="text-gray-500 italic">Talequah, OK</p>
 				<p class="text-gray-500 font-semibold">2024-2028</p>
 			</li>
 			<li class="mb-10 ms-4">
