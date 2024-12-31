b236be48011f1aa9b6677a8acc38ef63bd19dd43
jdang00 <justindang02@gmail.com>
2024-12-31 17:56:32 -0600

    a little more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div
-	class="mt-24 flex min-h-screen w-full flex-col items-center justify-between px-8 pb-8 text-center sm:px-0 sm:text-start"
+	class="mt-24 flex min-h-screen w-full flex-col items-center justify-between px-12 pb-8 text-center sm:px-0 sm:text-start"
 >
-	<div class="mt-18 w-full max-w-5xl">
+	<div class="mt-18 w-full max-w-3xl">
 		<div
 			class="flex flex-row flex-wrap items-center justify-center gap-12 border-b-2 pb-12 sm:justify-start"
 		>
@@ -17,18 +17,28 @@
 				<Avatar.Fallback>JD</Avatar.Fallback>
 			</Avatar.Root>
 			<div class="flex flex-col gap-y-2">
-				<h1 class="text-5xl font-semibold">Justin A. Dang</h1>
+				<h1 class="text-5xl font-medium">Justin A. Dang</h1>
 				<p>Optometry Student</p>
 			</div>
 		</div>
 
 		<div class="mt-8">
-			<h2 class="text-3xl font-semibold">About Me</h2>
-			<div class="mt-6 flex flex-col gap-y-2 text-start text-lg">
-				<p>Saved by grace through faith.</p>
-				<p>Optometry Student at Northeastern State University Oklahoma College of Optometry.</p>
-				<p>Developer with interests in generative AI and human computer interaction.</p>
-				<p>Building LearnTerms.com to enhance student studying.</p>
+			<h2 class="text-3xl font-medium">About Me</h2>
+			<div class=" mt-6 flex flex-col gap-y-2 text-start">
+				<p>
+					I'm Justin Dang, an optometry student at Northeastern State University Oklahoma College of
+					Optometry. Originally from Fort Smith Arkansas, I grew up with a strong background in the
+					humanities and the Great Books of the Western World. In college, I studied computer
+					science with artificial intelligence and human computer interaction.
+				</p>
+				<p>
+					My current interests are in using generative agents in the field of healthcare and health
+					education. That's why I'm building <a
+						href="https://www.learnterms.com"
+						target="_blank"
+						class="text-blue-500 underline">LearnTerms.com</a
+					> to enhance student studying.
+				</p>
 			</div>
 		</div>
 	</div>
